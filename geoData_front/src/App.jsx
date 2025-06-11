1b25c2d2b0d0cddd4e99857e6dddc35b4e4e8567
lanheiv <markussmpp@gmail.com>
2025-06-11 16:32:01 +0300

    small fix

--- geoData_front/src/App.jsx
+++ geoData_front/src/App.jsx
@@ -6,17 +6,14 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchCountries = async () => {
-      try {
-        const response = await fetch('http://127.0.0.1:8000/api/countries');
-        const data = await response.json();
-        setCountries(data);
-      } finally {
-        setLoading(false);
-      }
+    const fetchList = async () => {
+      const response = await fetch('http://127.0.0.1:8000/api/countries');
+      const data = await response.json();
+      setCountries(data);
+      setLoading(false);
     };
 
-    fetchCountries();
+    fetchList();
   }, []);
 
   return (
